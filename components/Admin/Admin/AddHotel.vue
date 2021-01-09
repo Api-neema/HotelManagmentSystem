c10b6a0bbad97c37bd8c5a1a7764f47a129f41ad
MinaOsam <osammina797@gmail.com>
2021-01-09 21:03:29 +0200

    added admin components

---
+++ components/Admin/Admin/AddHotel.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-form ref="form">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="hotelName"
+            label="Hotel name"
+            required
+            :rules="nameRules"
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12">
+          <v-text-field
+            v-model="hotelAddress"
+            label="Hotel Address"
+            required
+            :rules="nameRules"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field
+            v-model="singleRooms"
+            label="Number of single rooms"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="4">
+          <v-text-field
+            v-model="doubleRooms"
+            label="Number of double rooms"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="4">
+          <v-text-field
+            v-model="tripleRooms"
+            label="Number of triple rooms"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-btn style="background-color: #4caf50" class="mr-4" @click="submit">
+            Submit
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+
+<script>
+export default {
+  data: () => ({
+    hotelName: "",
+    hotelAddress: "",
+    singleRooms: "",
+    doubleRooms: "",
+    tripleRooms: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    valid: false,
+  }),
+  methods: {
+    submit: function () {
+      let hotel = {
+        hotelName: this.hotelName,
+        hotelAddress: this.hotelAddress,
+        singleRooms: this.singleRooms,
+        doubleRooms: this.doubleRooms,
+        tripleRooms: this.tripleRooms,
+      };
+      this.$refs.form.validate();
+      console.log(hotel);
+    },
+  },
+};
+</script>
---
+++ components/Admin/Admin/AddManager.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="3"> </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="3"> </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3"> </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="3"> </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3"> </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="password"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="3"> </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3"> </v-col>
+        <v-col cols="6">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="3"> </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="3"> </v-col>
+        <v-col cols="6">
+          <v-btn block depressed v-on:click="register">Register </v-btn>
+        </v-col>
+        <v-col cols="3"> </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      valid: false,
+      firstname: "",
+      lastname: "",
+      userType: "manager",
+      nameRules: [
+        (v) => !!v || "Name is required",
+        (v) => v.length <= 10 || "Name must be less than 10 characters",
+      ],
+      email: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      ],
+      password: "",
+    };
+  },
+  methods: {
+    register: function () {
+      console.log(`firstname: ${this.firstname}`);
+      console.log(`lastname: ${this.lastname}`);
+      console.log(`usertype: ${this.userType}`);
+      console.log(`email: ${this.email}`);
+      console.log(`password: ${this.password}`);
+    },
+  },
+};
+</script>
