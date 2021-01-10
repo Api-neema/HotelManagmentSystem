4ee6ad8750aacf61903cbad92c0489974a075973
omar hewady <omar.hewady@gmail.com>
2021-01-10 20:14:53 +0200

    added user components

---
+++ components/User/Profile.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-card height="100vh">
+    <v-row>
+      <v-col cols="2">
+        <v-navigation-drawer absolute permanent>
+          <template v-slot:prepend>
+            <v-list-item two-line>
+              <v-list-item-avatar>
+                <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title>Jane Smith</v-list-item-title>
+                <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+
+          <v-divider></v-divider>
+
+          <v-list dense>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title @click="showAccount"
+                  >My Account</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home-city</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title @click="showServices"
+                  >Services</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-email</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title @click="showSupport"
+                  >Support</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </v-col>
+      <v-col cols="10">
+        <Account v-if="account" />
+        <Services v-if="services" />
+        <Support v-if="support" />
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+<script>
+import Account from "./Account";
+import Services from "./Services";
+import Support from "./Support";
+
+export default {
+  components: {
+    Account,
+    Services,
+    Support,
+  },
+  data() {
+    return {
+      account: true,
+      services: false,
+      support: false,
+    };
+  },
+  methods: {
+    showAccount: function () {
+      this.account = true;
+      this.services = false;
+      this.support = false;
+    },
+    showServices: function () {
+      this.account = false;
+      this.services = true;
+      this.support = false;
+    },
+    showSupport: function () {
+      this.account = false;
+      this.services = false;
+      this.support = true;
+    },
+  },
+};
+</script>
+<style  scoped>
+.v-list-item__title {
+  margin-left: 10px;
+  cursor: pointer;
+}
+</style>
---
+++ components/User/Services.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-row v-if="Services.length == 0">
+      <v-col cols="6"> <p>You haven't subscribed to any services</p> </v-col>
+    </v-row>
+    <v-row>
+      <v-col v-for="item in Services" :key="item.name" cols="4">
+        <v-card class="mx-auto" max-width="400">
+          <v-img class="white--text align-end" height="200px" :src="item.image">
+          </v-img>
+
+          <v-card-subtitle class="pb-0"> {{ item.name }} </v-card-subtitle>
+
+          <v-card-text class="text--primary">
+            <div>{{ item.description }}</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="primary" style="color: green" text>
+              Cancel Subscription
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  data: () => ({
+    Services: [
+      {
+        name: "HouseKeeping",
+        description: "HouseKeeping",
+        image: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
+      },
+    ],
+  }),
+  methods: {},
+};
+</script>
